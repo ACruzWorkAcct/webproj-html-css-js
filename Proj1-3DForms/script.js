e102fa368f5868e9969fa6e0efb02ad444002404
Alejandro Cruz <AlejandroCruz@users.noreply.github.com>
2022-07-12 09:07:23 -0400

    Init

---
+++ Proj1-3DForms/script.js
@@ -0,0 +1,14 @@
+console.log("JS communication");
+
+const signUpBtn = document.querySelector(".signup-btn");
+const signInBtn = document.querySelector(".signin-btn");
+const formsWrapper = document.querySelector(".forms-wrapper");
+
+signUpBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formsWrapper.classList.add("change");
+});
+signInBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formsWrapper.classList.remove("change");
+});
